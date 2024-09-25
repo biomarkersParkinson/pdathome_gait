b31aecbabde7dd1286f05979b43089af26a797d8
Erikpostt <erik_post_6@hotmail.com>
2024-09-25 12:32:42 +0200

    Restructuring

--- src/pdathome/classification.py
+++ src/pdathome/classification.py
@@ -14,7 +14,7 @@ from typing import Callable, List
 
 from paradigma.gait_analysis_config import GaitFeatureExtractionConfig, ArmSwingFeatureExtractionConfig
 
-from pdathome.constants import classifiers, columns, descriptives, participant_ids, paths, classifier_hyperparameters
+from pdathome.constants import GlobalConstants as gc
 from pdathome.load import load_dataframes_directory
 from pdathome.utils import save_to_pickle
 
@@ -71,22 +71,22 @@ def train_test(
             step=step
         )
 
-        with open(os.path.join(paths.PATH_THRESHOLDS, step, f'{classifier_name}_{subject}.txt'), 'w') as f:
+        with open(os.path.join(gc.paths.PATH_THRESHOLDS, step, f'{classifier_name}_{subject}.txt'), 'w') as f:
             f.write(str(classification_threshold))
 
 
 def train_test_gait_detection(subject, l_classifiers, n_jobs):
     train_test(
         subject=subject,
-        l_ids=participant_ids.L_PD_IDS + participant_ids.L_HC_IDS,
+        l_ids=gc.participant_ids.L_PD_IDS + gc.participant_ids.L_HC_IDS,
         config_class=GaitFeatureExtractionConfig,
         l_classifiers=l_classifiers,
-        target_column_name=columns.GAIT_MAJORITY_VOTING,
-        pred_proba_colname=columns.PRED_GAIT_PROBA,
-        pred_colname=columns.PRED_GAIT,
+        target_column_name=gc.columns.GAIT_MAJORITY_VOTING,
+        pred_proba_colname=gc.columns.PRED_GAIT_PROBA,
+        pred_colname=gc.columns.PRED_GAIT,
         step='gait',
-        path_features=paths.PATH_GAIT_FEATURES,
-        path_predictions=paths.PATH_GAIT_PREDICTIONS,
+        path_features=gc.paths.PATH_GAIT_FEATURES,
+        path_predictions=gc.paths.PATH_GAIT_PREDICTIONS,
         n_jobs=n_jobs
     )
 
@@ -94,15 +94,15 @@ def train_test_gait_detection(subject, l_classifiers, n_jobs):
 def train_test_filtering_gait(subject, l_classifiers, n_jobs):
     train_test(
         subject=subject,
-        l_ids=participant_ids.L_PD_IDS,
+        l_ids=gc.participant_ids.L_PD_IDS,
         config_class=ArmSwingFeatureExtractionConfig,
         l_classifiers=l_classifiers,
-        target_column_name=columns.OTHER_ARM_ACTIVITY_MAJORITY_VOTING,
-        pred_proba_colname=columns.PRED_OTHER_ARM_ACTIVITY_PROBA,
-        pred_colname=columns.PRED_OTHER_ARM_ACTIVITY,
+        target_column_name=gc.columns.OTHER_ARM_ACTIVITY_MAJORITY_VOTING,
+        pred_proba_colname=gc.columns.PRED_OTHER_ARM_ACTIVITY_PROBA,
+        pred_colname=gc.columns.PRED_OTHER_ARM_ACTIVITY,
         step='arm_activity',
-        path_features=paths.PATH_ARM_ACTIVITY_FEATURES,
-        path_predictions=paths.PATH_ARM_ACTIVITY_PREDICTIONS,
+        path_features=gc.paths.PATH_ARM_ACTIVITY_FEATURES,
+        path_predictions=gc.paths.PATH_ARM_ACTIVITY_PREDICTIONS,
         n_jobs=n_jobs
     )
 
@@ -118,11 +118,11 @@ def cv_train_test_model(subject, df, classifier_name, l_predictors, l_predictors
     class_weight = None if step == 'gait' else 'balanced'
 
     # Split the data for training and testing
-    df_train = df[df[columns.ID] != subject].copy()
-    df_test = df[df[columns.ID] == subject].copy()
+    df_train = df[df[gc.columns.ID] != subject].copy()
+    df_test = df[df[gc.columns.ID] == subject].copy()
     
     # Fit a scaler on the pd participants
-    pd_train_mask = df_train[columns.ID].isin(participant_ids.L_PD_IDS)
+    pd_train_mask = df_train[gc.columns.ID].isin(gc.participant_ids.L_PD_IDS)
     scaler = StandardScaler()
 
     # Scale the data
@@ -136,15 +136,15 @@ def cv_train_test_model(subject, df, classifier_name, l_predictors, l_predictors
     y_test = df_test[target_column_name].astype(int)
 
     # Initialize the model
-    if classifier_name == classifiers.RANDOM_FOREST:
+    if classifier_name == gc.classifiers.RANDOM_FOREST:
         clf = RandomForestClassifier(
-            **classifier_hyperparameters[classifiers.RANDOM_FOREST],
+            **gc.classifiers.RANDOM_FOREST_HYPERPARAMETERS,
             class_weight=class_weight,
             n_jobs=n_jobs
         )
-    elif classifier_name == classifiers.LOGISTIC_REGRESSION:
+    elif classifier_name == gc.classifiers.LOGISTIC_REGRESSION:
         clf = LogisticRegression(
-            **classifier_hyperparameters[classifiers.LOGISTIC_REGRESSION],
+            **gc.classifiers.LOGISTIC_REGRESSION_HYPERPARAMETERS,
             class_weight=class_weight,
             n_jobs=n_jobs,
         )
@@ -180,40 +180,40 @@ def windows_to_timestamps(subject, df, path_output, pred_proba_colname, step):
     if step not in ['gait', 'arm_activity']:
         raise ValueError("Step not recognized")
 
-    # Define base columns
-    l_subj_cols = [columns.SIDE, columns.WINDOW_NR, pred_proba_colname]
-    l_merge_ts_cols = [columns.WINDOW_NR, columns.SIDE]
-    l_merge_points_cols = [columns.TIME, columns.SIDE]
-    l_groupby_cols = [columns.TIME, columns.SIDE]
-    l_explode_cols = [columns.TIME]
-    l_dupl_cols = [columns.TIME, columns.SIDE]
+    # Define base gc.columns
+    l_subj_cols = [gc.columns.SIDE, gc.columns.WINDOW_NR, pred_proba_colname]
+    l_merge_ts_cols = [gc.columns.WINDOW_NR, gc.columns.SIDE]
+    l_merge_points_cols = [gc.columns.TIME, gc.columns.SIDE]
+    l_groupby_cols = [gc.columns.TIME, gc.columns.SIDE]
+    l_explode_cols = [gc.columns.TIME]
+    l_dupl_cols = [gc.columns.TIME, gc.columns.SIDE]
 
-    # Add PD-specific columns
-    if subject in participant_ids.L_PD_IDS:
-        pd_specific_cols = [columns.PRE_OR_POST, columns.ARM_LABEL]
-        l_subj_cols.append(columns.PRE_OR_POST)
+    # Add PD-specific gc.columns
+    if subject in gc.participant_ids.L_PD_IDS:
+        pd_specific_cols = [gc.columns.PRE_OR_POST, gc.columns.ARM_LABEL]
+        l_subj_cols.append(gc.columns.PRE_OR_POST)
         l_groupby_cols += pd_specific_cols
-        l_explode_cols.append(columns.ARM_LABEL)
-        l_merge_ts_cols.append(columns.PRE_OR_POST)
-        l_merge_points_cols.append(columns.PRE_OR_POST)
-        l_dupl_cols.append(columns.PRE_OR_POST)
+        l_explode_cols.append(gc.columns.ARM_LABEL)
+        l_merge_ts_cols.append(gc.columns.PRE_OR_POST)
+        l_merge_points_cols.append(gc.columns.PRE_OR_POST)
+        l_dupl_cols.append(gc.columns.PRE_OR_POST)
 
     if step == 'gait':
-        l_subj_cols += [columns.GAIT_MAJORITY_VOTING, columns.ACTIVITY_LABEL_MAJORITY_VOTING]
-        path_features = paths.PATH_GAIT_FEATURES
-        l_explode_cols.append(columns.FREE_LIVING_LABEL)
-        l_groupby_cols.append(columns.FREE_LIVING_LABEL)
+        l_subj_cols += [gc.columns.GAIT_MAJORITY_VOTING, gc.columns.ACTIVITY_LABEL_MAJORITY_VOTING]
+        path_features = gc.paths.PATH_GAIT_FEATURES
+        l_explode_cols.append(gc.columns.FREE_LIVING_LABEL)
+        l_groupby_cols.append(gc.columns.FREE_LIVING_LABEL)
     elif step == 'arm_activity':
-        path_features = paths.PATH_ARM_ACTIVITY_FEATURES
-        if subject in participant_ids.L_PD_IDS:
-            l_subj_cols += [columns.OTHER_ARM_ACTIVITY_MAJORITY_VOTING, columns.ARM_LABEL_MAJORITY_VOTING]
+        path_features = gc.paths.PATH_ARM_ACTIVITY_FEATURES
+        if subject in gc.participant_ids.L_PD_IDS:
+            l_subj_cols += [gc.columns.OTHER_ARM_ACTIVITY_MAJORITY_VOTING, gc.columns.ARM_LABEL_MAJORITY_VOTING]
 
-    # Select relevant columns
+    # Select relevant gc.columns
     df = df[l_subj_cols]     
     
     # Load and combine timestamps data
-    df_ts_mas = pd.read_pickle(os.path.join(path_features, f'{subject}_{descriptives.MOST_AFFECTED_SIDE}_ts.pkl')).assign(side=descriptives.MOST_AFFECTED_SIDE)
-    df_ts_las = pd.read_pickle(os.path.join(path_features, f'{subject}_{descriptives.LEAST_AFFECTED_SIDE}_ts.pkl')).assign(side=descriptives.LEAST_AFFECTED_SIDE)
+    df_ts_mas = pd.read_pickle(os.path.join(path_features, f'{subject}_{gc.descriptives.MOST_AFFECTED_SIDE}_ts.pkl')).assign(side=gc.descriptives.MOST_AFFECTED_SIDE)
+    df_ts_las = pd.read_pickle(os.path.join(path_features, f'{subject}_{gc.descriptives.LEAST_AFFECTED_SIDE}_ts.pkl')).assign(side=gc.descriptives.LEAST_AFFECTED_SIDE)
 
     df_ts = pd.concat([df_ts_mas, df_ts_las], ignore_index=True)
 
@@ -226,7 +226,7 @@ def windows_to_timestamps(subject, df, path_output, pred_proba_colname, step):
     # Reset index after merging
     df_single_points.reset_index(drop=True, inplace=True)
 
-    # Group by relevant columns and calculate mean prediction probability
+    # Group by relevant gc.columns and calculate mean prediction probability
     if step == 'gait':
         df_pred_per_point = df_single_points.groupby(l_groupby_cols)[pred_proba_colname].mean().reset_index()
     else:
@@ -250,11 +250,11 @@ def store_model_output(df, classifier_name, step, n_jobs=-1):
     # Determine class weight based on the step
     if step == 'gait':
         class_weight = None
-        target_column_name = columns.GAIT_MAJORITY_VOTING
+        target_column_name = gc.columns.GAIT_MAJORITY_VOTING
         config = GaitFeatureExtractionConfig()
     else:
         class_weight = 'balanced'
-        target_column_name = columns.OTHER_ARM_ACTIVITY_MAJORITY_VOTING
+        target_column_name = gc.columns.OTHER_ARM_ACTIVITY_MAJORITY_VOTING
         config = ArmSwingFeatureExtractionConfig()
 
     l_predictors = list(config.d_channels_values.keys())
@@ -262,7 +262,7 @@ def store_model_output(df, classifier_name, step, n_jobs=-1):
 
     # Standardize features based on the PD subjects    
     scaler = StandardScaler()
-    df_pd = df.loc[df[columns.ID].isin(participant_ids.L_PD_IDS), l_predictors_scaled]
+    df_pd = df.loc[df[gc.columns.ID].isin(gc.participant_ids.L_PD_IDS), l_predictors_scaled]
     df_scaled = df.copy()
     
     scaler.fit(df_pd[l_predictors_scaled])
@@ -272,15 +272,15 @@ def store_model_output(df, classifier_name, step, n_jobs=-1):
     y = df_scaled[target_column_name].astype(int)
 
     # train on all subjects and store model
-    if classifier_name == classifiers.RANDOM_FOREST:
+    if classifier_name == gc.classifiers.RANDOM_FOREST:
         clf = RandomForestClassifier(
-            **classifier_hyperparameters[classifiers.RANDOM_FOREST],
+            **gc.classifiers.RANDOM_FOREST_HYPERPARAMETERS,
             class_weight=class_weight,
             n_jobs=n_jobs
         )
-    elif classifier_name == classifiers.LOGISTIC_REGRESSION:
+    elif classifier_name == gc.classifiers.LOGISTIC_REGRESSION:
         clf = LogisticRegression(
-            **classifier_hyperparameters[classifiers.LOGISTIC_REGRESSION],
+            **gc.classifiers.LOGISTIC_REGRESSION_HYPERPARAMETERS,
             class_weight=class_weight,
             n_jobs=n_jobs
         )
@@ -289,16 +289,16 @@ def store_model_output(df, classifier_name, step, n_jobs=-1):
     clf.fit(X, y)
 
     # Save the model as a pickle file
-    model_path = os.path.join(paths.PATH_CLASSIFIERS, step, f'{classifier_name}.pkl')
+    model_path = os.path.join(gc.paths.PATH_CLASSIFIERS, step, f'{classifier_name}.pkl')
 
-    if not os.path.exists(paths.PATH_CLASSIFIERS):
-        os.makedirs(paths.PATH_CLASSIFIERS)
+    if not os.path.exists(gc.paths.PATH_CLASSIFIERS):
+        os.makedirs(gc.paths.PATH_CLASSIFIERS)
 
     with open(model_path, 'wb') as f:
         pickle.dump(clf, f)
 
     # Save coefficients as json
-    with open(os.path.join(paths.PATH_COEFFICIENTS, step, f'{classifier_name}.json'), 'w') as f:
+    with open(os.path.join(gc.paths.PATH_COEFFICIENTS, step, f'{classifier_name}.json'), 'w') as f:
         if type(clf).__name__.lower() == 'logisticregression': 
             coefficients = {k: v for k, v in zip(l_predictors, clf.coef_[0])}
         elif type(clf).__name__.lower() == 'randomforestclassifier':
@@ -310,21 +310,21 @@ def store_model_output(df, classifier_name, step, n_jobs=-1):
     thresholds = []
     
     if step == 'gait':
-        l_ids = participant_ids.L_PD_IDS + participant_ids.L_HC_IDS
+        l_ids = gc.participant_ids.L_PD_IDS + gc.participant_ids.L_HC_IDS
     else:
-        l_ids = participant_ids.L_PD_IDS 
+        l_ids = gc.participant_ids.L_PD_IDS 
 
     for subject in l_ids:
-        with open(os.path.join(paths.PATH_THRESHOLDS, step, f'{classifier_name}_{subject}.txt'), 'r') as f:
+        with open(os.path.join(gc.paths.PATH_THRESHOLDS, step, f'{classifier_name}_{subject}.txt'), 'r') as f:
             thresholds.append(float(f.read()))
 
     mean_threshold = np.mean(thresholds)
-    with open(os.path.join(paths.PATH_THRESHOLDS, step, f'{classifier_name}.txt'), 'w') as f:
+    with open(os.path.join(gc.paths.PATH_THRESHOLDS, step, f'{classifier_name}.txt'), 'w') as f:
         f.write(str(mean_threshold))
 
     # Delete individual thresholds
     for subject in l_ids:
-        os.remove(os.path.join(paths.PATH_THRESHOLDS, step, f'{classifier_name}_{subject}.txt')) 
+        os.remove(os.path.join(gc.paths.PATH_THRESHOLDS, step, f'{classifier_name}_{subject}.txt')) 
 
     # Save the scaler parameters as JSON
     scaler_params = {
@@ -333,10 +333,10 @@ def store_model_output(df, classifier_name, step, n_jobs=-1):
         'scale': scaler.scale_.tolist()
     }
 
-    scaler_path = os.path.join(paths.PATH_SCALERS, step, f'scaler_params.json')
+    scaler_path = os.path.join(gc.paths.PATH_SCALERS, step, f'scaler_params.json')
 
-    if not os.path.exists(paths.PATH_SCALERS):
-        os.makedirs(paths.PATH_SCALERS)
+    if not os.path.exists(gc.paths.PATH_SCALERS):
+        os.makedirs(gc.paths.PATH_SCALERS)
         
     with open(scaler_path, 'w') as f:
         json.dump(scaler_params, f)
@@ -347,29 +347,29 @@ def store_model_output(df, classifier_name, step, n_jobs=-1):
 
 
 def predict_controls(clf, scaler, classifier_name, l_predictors, l_predictors_scaled, step):
-    for subject in participant_ids.L_HC_IDS:
-        df_subj_mas = pd.read_pickle(os.path.join(paths.PATH_ARM_ACTIVITY_FEATURES, f'{subject}_{descriptives.MOST_AFFECTED_SIDE}.pkl'))
-        df_subj_las = pd.read_pickle(os.path.join(paths.PATH_ARM_ACTIVITY_FEATURES, f'{subject}_{descriptives.LEAST_AFFECTED_SIDE}.pkl'))
-        df_subj_mas['side'] = descriptives.MOST_AFFECTED_SIDE
-        df_subj_las['side'] = descriptives.LEAST_AFFECTED_SIDE
+    for subject in gc.participant_ids.L_HC_IDS:
+        df_subj_mas = pd.read_pickle(os.path.join(gc.paths.PATH_ARM_ACTIVITY_FEATURES, f'{subject}_{gc.descriptives.MOST_AFFECTED_SIDE}.pkl'))
+        df_subj_las = pd.read_pickle(os.path.join(gc.paths.PATH_ARM_ACTIVITY_FEATURES, f'{subject}_{gc.descriptives.LEAST_AFFECTED_SIDE}.pkl'))
+        df_subj_mas['side'] = gc.descriptives.MOST_AFFECTED_SIDE
+        df_subj_las['side'] = gc.descriptives.LEAST_AFFECTED_SIDE
         df_subj = pd.concat([df_subj_mas, df_subj_las]).reset_index(drop=True)
 
         df_subj[l_predictors_scaled] = scaler.transform(df_subj[l_predictors_scaled])
-        df_subj[columns.PRED_OTHER_ARM_ACTIVITY_PROBA] = clf.predict_proba(df_subj[l_predictors])[:,1]
-        df_subj[columns.PRED_OTHER_ARM_ACTIVITY] = clf.predict(df_subj[l_predictors])
+        df_subj[gc.columns.PRED_OTHER_ARM_ACTIVITY_PROBA] = clf.predict_proba(df_subj[l_predictors])[:,1]
+        df_subj[gc.columns.PRED_OTHER_ARM_ACTIVITY] = clf.predict(df_subj[l_predictors])
 
         windows_to_timestamps(
             subject=subject, df=df_subj,
-            path_output=os.path.join(paths.PATH_ARM_ACTIVITY_PREDICTIONS, classifier_name),
-            pred_proba_colname=columns.PRED_OTHER_ARM_ACTIVITY_PROBA,
+            path_output=os.path.join(gc.paths.PATH_ARM_ACTIVITY_PREDICTIONS, classifier_name),
+            pred_proba_colname=gc.columns.PRED_OTHER_ARM_ACTIVITY_PROBA,
             step=step
         )
 
 
 def store_gait_detection(l_classifiers, n_jobs=-1):
     df = load_dataframes_directory(
-        directory_path=paths.PATH_GAIT_FEATURES,
-        l_ids=participant_ids.L_PD_IDS + participant_ids.L_HC_IDS
+        directory_path=gc.paths.PATH_GAIT_FEATURES,
+        l_ids=gc.participant_ids.L_PD_IDS + gc.participant_ids.L_HC_IDS
     )
 
     for classifier in l_classifiers:
@@ -383,8 +383,8 @@ def store_gait_detection(l_classifiers, n_jobs=-1):
 
 def store_filtering_gait(l_classifiers, n_jobs=-1):
     df = load_dataframes_directory(
-        directory_path=paths.PATH_ARM_ACTIVITY_FEATURES,
-        l_ids=participant_ids.L_PD_IDS,
+        directory_path=gc.paths.PATH_ARM_ACTIVITY_FEATURES,
+        l_ids=gc.participant_ids.L_PD_IDS,
     )
 
     for classifier in l_classifiers:
