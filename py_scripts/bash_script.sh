b191796fec0b57eb3ea555acc4f62c510abaf837
Erik Post <erik_post_6@hotmail.com>
2024-09-25 08:17:02 +0200

    Simplifying structure

--- py_scripts/bash_script.sh
+++ py_scripts/bash_script.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH -t 03:00:00
 #SBATCH -p genoa
-#SBATCH -o ./slurm/output.%j.out # STDOUT
+#SBATCH -o ../slurm/output.%j.out # STDOUT
 
 #Loading modules
 module load 2023
@@ -14,7 +14,7 @@ source $(poetry env info --path)/bin/activate
 
 #Run same program over many different inputs
 nproc=3
-steps=2345
+steps=345
 input_ids=$(cat ../../id_files/all.txt)
 
 python -u run_pipeline.py $nproc $steps $input_ids
