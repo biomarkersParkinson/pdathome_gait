27815102a204b21b82941707db9813d1aa44c3ec
Erik Post <erik_post_6@hotmail.com>
2024-10-01 14:26:03 +0200

    Solved paralellization error

--- paper_results/generate_results/generate_results.py
+++ paper_results/generate_results/generate_results.py
@@ -10,10 +10,17 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 )
 
+
+def wrapper_function(subject_step):
+    subject, step = subject_step
+    return generate_results(subject, step)
+
+
 def parallelize_function(n_processes, l_ids, func, step):
     with Pool(int(n_processes)) as p:
         try:
-            results = p.map(lambda subject: func(subject, step), l_ids)     
+            subject_steps = [(subject, step) for subject in l_ids]
+            results = p.map(wrapper_function, subject_steps)
             return results
         except Exception:
             logging.exception("Exception occurred during parallel processing.")
@@ -30,7 +37,7 @@ if __name__ == '__main__':
     for step in l_steps:
         results = parallelize_function(nproc, l_ids, generate_results, step)
         if results is not None:
-            combined_results[step] = dict(zip(l_ids, results))
+            combined_results[step] = {id_: result for id_, result in zip(l_ids, results) if result is not None}
 
     # Store the combined results in a JSON file
     with open('combined_results.json', 'w') as f:
