f244d9e6d12f83873aeb4a60ec6b3462c2b2a6b6
Erikpostt <erik_post_6@hotmail.com>
2024-10-03 11:02:43 +0200

    Swap

--- pipeline/py_scripts/bash_script.sh
+++ pipeline/py_scripts/bash_script.sh
@@ -7,17 +7,20 @@
 # Get the current date and time for the output file
 timestamp=$(date +%Y%m%d_%H%M%S)
 
-# Set output file with timestamp
-#SBATCH -o ../../slurm/output_%x_%j_${timestamp}.out
+# Define the output file with the timestamp
+output_file="../../slurm/output_${SLURM_JOB_NAME}_${SLURM_JOB_ID}_${timestamp}.out"
 
-#Loading modules
+# Run the sbatch command and specify the output file
+#SBATCH --output=$output_file 
+
+# Load modules
 module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
 
 # Activate Poetry environment
 source $(poetry env info --path)/bin/activate
 
-#Run same program over many different inputs
+# Run the program
 nproc=8
 steps=3456
 input_ids=$(cat ../../id_files/all.txt)
