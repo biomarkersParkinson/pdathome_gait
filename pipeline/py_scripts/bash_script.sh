6bb67e9028e567a73f5a27d8850183aeaa06b684
Erikpostt <erik_post_6@hotmail.com>
2024-10-04 16:00:29 +0200

    Swap

--- pipeline/py_scripts/bash_script.sh
+++ pipeline/py_scripts/bash_script.sh
@@ -16,7 +16,7 @@ source $(poetry env info --path)/bin/activate
 
 # Run the program
 nproc=8
-steps=5
+steps=567
 input_ids=$(cat ../../id_files/all.txt)
 
 python -u run_pipeline.py $nproc $steps $input_ids
